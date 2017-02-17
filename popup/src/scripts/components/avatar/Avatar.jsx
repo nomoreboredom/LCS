eb91e95e18703e5e9c922e13ff82d56634f59374
Arda Ilgaz <Arda Ilgaz>
2017-02-17 22:44:46 +0300

    Channel Avatar Added, eslint Custumized

--- popup/src/scripts/components/avatar/Avatar.jsx
+++ popup/src/scripts/components/avatar/Avatar.jsx
@@ -1,32 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Avatar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      src: this.props.src,
-      size: this.props.size,
-      round: this.props.name,
-  };
-  }
-  render() {
-      const size = this.props.size;
-      const round = this.props.round;
-      const alt = this.props.name;
-      const imageStyle = {
-          maxWidth: '100%',
-          width: size,
-          height: size,
-          borderRadius: (round ? 500 : 0)
-      };
-      return (
-          <img width={this.props.size}
+    constructor(props) {
+        super(props);
+        this.state = {
+            src: this.props.src,
+            size: this.props.size,
+            round: this.props.name,
+        };
+    }
+    render() {
+        const size = this.props.size;
+        const round = this.props.round;
+        const alt = this.props.name;
+        const imageStyle = {
+            maxWidth: '100%',
+            width: size,
+            height: size,
+            borderRadius: (round ? 500 : 0),
+        };
+        return (
+            <img
+              width={this.props.size}
               height={this.props.size}
               style={imageStyle}
               src={this.state.src}
               alt={alt}
-              onError={this.fetch} />
-      );
-  }
+              onError={this.fetch}
+            />
+        );
+    }
 }
-export default Avatar
+
+Avatar.defaultProps = {
+    src: '',
+    size: 100,
+    name: 'Channel',
+    round: false,
+};
+
+Avatar.propTypes = {
+    src: PropTypes.string,
+    size: PropTypes.number,
+    name: PropTypes.name,
+    round: PropTypes.bool,
+};
+
+export default Avatar;
